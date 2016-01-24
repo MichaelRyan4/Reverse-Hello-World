dd1c4ff369f8ee601715171a825d4ac61e43d0dc
Michael Ryan <MichaelRyan@Michaels-MacBook-Pro.local>
2016-01-24 02:05:25 -0500

    Initial Commit

---
+++ winner.py
@@ -0,0 +1,8 @@
+def reverse(text):
+    num = len(text) - 1
+    rev = []
+    for i in range(-1, num):
+        rev.append(text[num])
+        num -= 1
+    return ''.join(rev)
+print reverse('.iafiralc ot woh erus etiuq t\'nsaW .aremac wen a esu dluoc I ,segduj em no ytip evaH .margorp tsrif ym si siht ,dlrow olleH')
